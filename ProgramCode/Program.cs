484dc27bdbd33101ff474fe7d1e9399c920f19ce
VadimN <vadimn1917@gmail.com>
2022-09-05 13:01:56 +0600

    ProgramCode

---
+++ ProgramCode/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите длинну массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводите строки");
+string[] array = new string[n];
+for(int i = 0; i < n; i++){
+array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+for(int i = 0; i < n; i++){
+    if(array [i].Length <= 3)
+Console.Write("[{0}]", string.Join(", ", array[i]));
+}
